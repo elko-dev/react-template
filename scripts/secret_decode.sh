33c2b0d32073135f0a3a1e5a82d7bd8b46f2f082
spawn-dev <spawn-dev>
2020-04-06 10:00:24 -0500

    add firebase auth

---
+++ scripts/secret_decode.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_DIR="$SCRIPT_DIR/.."
+CONFIG_DIR="$SCRIPT_DIR/../config"
+
+if [[ -z "${AUTH_CONFIG}" ]]; then
+  echo "No firebase config found..."
+else
+  rm -rf "$CONFIG_DIR"
+  mkdir "$CONFIG_DIR"
+  echo "Firebase config found, decoding configuration..."
+  echo "${AUTH_CONFIG}" | base64 --decode >"$CONFIG_DIR/spawn-platform.json"
+fi
