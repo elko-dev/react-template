026bfe661c43ccc6ac007d3da90152b1af72a691
spawn-dev <spawn-dev>
2020-04-19 23:05:43 -0500

    add deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,17 @@
 
 set -euo pipefail
 
-dpl --provider=heroku --app=$APP_NAME --api-key=$HEROKU_API_KEY
+APP_NAME=$1
+ENV=$2
+
+if [ "$ENV" != "dev" ] && [ "$ENV" != "stage" ] && [ "$ENV" != "prod" ]
+then
+    echo "$ENV is not a valid environment, must be either dev, stage, or prod"
+    exit 1
+else 
+    echo "Deploying to $ENV..."
+fi
+
+gem install dpl
+
+dpl --provider=heroku --app="$APP_NAME" --api-key=$HEROKU_API_KEY
