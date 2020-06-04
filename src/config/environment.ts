726c6ab248502eb608d0b6e80b55886aad343c15
spawn-dev <spawn-dev>
2020-06-04 00:26:11 -0500

    dynamic url env

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,4 +1,26 @@
-export const environment = {
-  //apiBase: 'https://elko-trail-addict-server-dev.herokuapp.com',
-  apiBase: process.env.REACT_APP_API_BASE_PATH || 'http://localhost:4000',
+const key: string | undefined = process.env.REACT_APP_ENV;
+
+type urlOptions = {
+  [key: string]: string;
 };
+const urlMap: urlOptions = {
+  local: 'localhost:4000',
+  dev: '<DEV_BASE_URL>',
+  stage: '<STAGE_BASE_URL>',
+  prod: '<PROD_BASE_URL>',
+};
+
+export const environment = {
+  apiBase: getUrl(),
+};
+
+function getUrl(): string {
+  if (!key) {
+    console.error(
+      'REACT_APP_ENV variable was undefined.  We need this to select the correct environment specific variables'
+    );
+    process.exit(1);
+  }
+  console.log(key);
+  return urlMap[key];
+}
